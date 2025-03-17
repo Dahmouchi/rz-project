48bea57b45f489ff5daf06293eb0567377f5d611
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-03-17 21:57:22 +0000

    first section

--- src/app/_components/landing/Quotes.tsx
+++ src/app/_components/landing/Quotes.tsx
@@ -16,14 +16,31 @@ const Quotes = () => {
             className="lg:w-1/3 md:w-1/2 h-auto rounded-xl"
           />
           <div className="flex flex-col gap-4 lg:px-32 md:px-10">
-            <h1 className="lg:text-7xl text-second text-4xl ">Rz Hospitality</h1>
-            <h1 className="text-slate-100 lg:text-xl text-md">
-              ”Founded in 2023 By Ryda Zarhouni, we are dedicated to empowering
-              hospitality business owners in need of expert guidance in areas
-              such commercial strategy, operational management Recruitement, and
-              strategic planning.”
+            <h1 className="lg:text-5xl text-second text-4xl ">
+              Welcome to Rz Hospitality{" "}
             </h1>
-            <h1 className="text-right lg:text-2xl text-xl text-slate-50">- Ryda ZARHOUNI</h1>
+            <h1 className="text-slate-100 lg:text-xl text-md">
+              ”Founded in 2023 by Ryda Zarhouni, Rz Hospitality is your trusted
+              partner for elevating your business in the hospitality industry.
+              We specialize in delivering expert support in{" "}
+              <span className="font-bold">
+                Interim Management, Recruitment, Commercial Strategy, and
+                Operational Management.
+              </span>
+            </h1>
+            <h1 className="text-slate-100 lg:text-xl text-md">
+              Whether you need to optimize your operations, build a top-tier
+              team, or develop a winning business plan, we&apos;re here to guide
+              you every step of the way. Our goal is to help you create
+              exceptional experiences while driving growth and success.
+            </h1>
+            <h1 className="text-slate-100 lg:text-xl text-md">
+              Ready to transform your hospitality business? Let&apos;s make it
+              happen together!  ”
+            </h1>
+            <h1 className="text-right lg:text-2xl text-xl text-slate-50">
+              - Ryda ZARHOUNI
+            </h1>
           </div>
         </div>
       </div>
